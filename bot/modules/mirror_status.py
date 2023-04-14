fb21ffc6050ad9df6e1320685b4d7a7657ce6c54
anant-goel <100245626+anant-goel@users.noreply.github.com>
2023-04-14 09:57:39 +0530

    Update mirror_status.py

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -1,57 +1,48 @@
+#!/usr/bin/env python3
+from pyrogram.handlers import MessageHandler, CallbackQueryHandler
+from pyrogram.filters import command, regex
 from psutil import cpu_percent, virtual_memory, disk_usage
 from time import time
-from threading import Thread
-from pyrogram.handlers import MessageHandler, CallbackQueryHandler
-from telegram.ext import CommandHandler, CallbackQueryHandler
-from bot import status_reply_dict, status_reply_dict_lock, download_dict, download_dict_lock, botStartTime, DOWNLOAD_DIR, Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL, bot
-from bot.helper.telegram_helper.message_utils import sendMessage, deleteMessage, auto_delete_message, sendStatusMessage, update_all_messages
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time, turn_page, setInterval
-from bot.helper.telegram_helper.filters import CustomFilters 
+
+from bot import status_reply_dict_lock, download_dict, download_dict_lock, botStartTime, DOWNLOAD_DIR, Interval, config_dict, bot
+from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from pyrogram.filters import command, regex
+from bot.helper.telegram_helper.message_utils import sendMessage, deleteMessage, auto_delete_message, sendStatusMessage, update_all_messages
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time, turn_page, setInterval, new_task
 
 
-def mirror_status(update, context):
-    with download_dict_lock:
+@new_task
+async def mirror_status(client, message):
+    async with download_dict_lock:
         count = len(download_dict)
     if count == 0:
-            currentTime = get_readable_time(time() - botStartTime)
-            free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
-            message = 'Nothing To Do !\n_______________'
-            message += f"\n\n<b>CPU        :</b> {cpu_percent()}%\n<b>SSD        :</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}" \
-                       f"\n<b>RAM       :</b> {virtual_memory().percent}%\n<b>UPTM     :</b> {get_readable_time(time() - botStartTime)}"
-            reply_message = sendMessage(message, context.bot, update.message)
-            Thread(target=auto_delete_message, args=(context.bot, update.message, reply_message)).start()
+        currentTime = get_readable_time(time() - botStartTime)
+        free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
+        msg = 'No Active Downloads !\n___________________________'
+        msg += f"\n<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {free}" \
+            f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {currentTime}"
+        reply_message = await sendMessage(message, msg)
+        await auto_delete_message(message, reply_message)
     else:
-        index = update.effective_chat.id
-        with status_reply_dict_lock:
-            if index in status_reply_dict:
-                deleteMessage(context.bot, status_reply_dict[index][0])
-                del status_reply_dict[index]
-            try:
-                if Interval:
-                    Interval[0].cancel()
-                    Interval.clear()
-            except:
-                pass
-            finally:
-                Interval.append(setInterval(DOWNLOAD_STATUS_UPDATE_INTERVAL, update_all_messages))
-        sendStatusMessage(update.message, context.bot)
-        deleteMessage(context.bot, update.message)
+        await sendStatusMessage(message)
+        await deleteMessage(message)
+        async with status_reply_dict_lock:
+            if Interval:
+                Interval[0].cancel()
+                Interval.clear()
+                Interval.append(setInterval(
+                    config_dict['STATUS_UPDATE_INTERVAL'], update_all_messages))
 
-def status_pages(update, context):
-    query = update.callback_query
-    with status_reply_dict_lock:
-        if not status_reply_dict or not Interval or time() - list(status_reply_dict.values())[0][1] < 2:
-            query.answer(text="Wait One More Second!", show_alert=True)
-            return
-    data = query.data
-    data = data.split()
-    query.answer()
-    if done := turn_page(data):
-        update_all_messages(True)
+
+@new_task
+async def status_pages(client, query):
+    await query.answer()
+    data = query.data.split()
+    if data[1] == "ref":
+        await update_all_messages(True)
     else:
-        query.message.delete()
+        await turn_page(data)
+
 
 bot.add_handler(MessageHandler(mirror_status, filters=command(
     BotCommands.StatusCommand) & CustomFilters.authorized))
